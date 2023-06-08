679a7148e87655fbc17e72cc0d3018c06c206bfc
unknown <zainazharul@gmail.com>
2023-06-09 00:20:29 +0700

    feat: create loading atom component

---
+++ src/components/Atom/Loading/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="z-50 absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 rounded-full bg-red-darker">
+      <div className="border-t-red-flix border-solid animate-spin  rounded-full border-red-flix border-8 w-16 h-16 border-red-950"></div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/Atom/index.jsx
+++ src/components/Atom/index.jsx
@@ -1,2 +1,3 @@
+export { default as Loading } from "src/components/Atom/Loading";
 export { default as Button } from "src/components/Atom/Button";
 export { default as Logo } from "src/components/Atom/Logo";
