a331b7b2c985e4091ad7bfb3df11396e7a49e561
unknown <zainazharul@gmail.com>
2023-06-09 00:32:34 +0700

    refactor: remove duplicate loading border color

--- src/components/Atom/Loading/index.jsx
+++ src/components/Atom/Loading/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="z-50 absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 rounded-full bg-red-darker">
-      <div className="border-t-red-flix border-solid animate-spin  rounded-full border-red-flix border-8 w-16 h-16 border-red-950"></div>
+      <div className="border-t-red-flix border-solid animate-spin rounded-full border-8 w-16 h-16 border-red-950"></div>
     </div>
   );
 };
