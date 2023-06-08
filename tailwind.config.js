7ed074c0653a62d67948a09e213970f7bb39498c
unknown <zainazharul@gmail.com>
2023-06-08 22:43:22 +0700

    chore: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "red-flix": "#e50914",
+        "red-dark": "#c11119",
+        "red-darker": "#99161d",
+        "blue-download": "#0071eb",
+      },
+    },
   },
   plugins: [],
 };
