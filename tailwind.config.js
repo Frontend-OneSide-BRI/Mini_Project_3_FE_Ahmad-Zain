23b373ff1a2df809ef1db8f03c59d534bf7c2d5b
unknown <zainazharul@gmail.com>
2023-06-08 22:44:23 +0700

    chore: add custom background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ export default {
         "red-darker": "#99161d",
         "blue-download": "#0071eb",
       },
+      backgroundImage: {
+        herohome:
+          "url('https://assets.nflxext.com/ffe/siteui/vlv3/76c10fc9-7ccd-4fbf-bc59-f16a468921ca/f511e96e-c4cd-49b1-9fbf-ca9d78e15b50/ID-en-20230529-popsignuptwoweeks-perspective_alpha_website_small.jpg')",
+      },
     },
   },
   plugins: [],
