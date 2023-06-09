722c6cb9226e2ff89b9d92b52b8cf1c432d8744d
unknown <zainazharul@gmail.com>
2023-06-09 14:25:01 +0700

    feat: export formsearch, genres, and card molecule component

--- src/components/Molecule/index.jsx
+++ src/components/Molecule/index.jsx
@@ -1,2 +1,5 @@
+export { default as FormSearch } from "src/components/Molecule/Form/Search";
 export { default as Navbar } from "src/components/Molecule/Navbar";
 export { default as Footer } from "src/components/Molecule/Footer";
+export { default as Genres } from "src/components/Molecule/Genres";
+export { default as Card } from "src/components/Molecule/Card";
