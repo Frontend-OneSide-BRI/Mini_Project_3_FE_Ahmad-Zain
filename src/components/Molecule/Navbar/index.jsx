659e0b96bd570402a2121bc0f1c3020e221359a5
unknown <zainazharul@gmail.com>
2023-06-09 14:22:26 +0700

    feat: implement responsive design in navbar

--- src/components/Molecule/Navbar/index.jsx
+++ src/components/Molecule/Navbar/index.jsx
@@ -5,7 +5,7 @@ import Button from "src/components/Atom/Button";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center px-36 py-2">
+    <div className="flex justify-between items-center px-2 lg:px-36 py-2">
       <Logo />
       <Button text="Sign In" className="text-sm w-20 h-8" />
     </div>
