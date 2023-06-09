bb6794302bd03d7318b38ac614fb60bbc8b7c242
unknown <zainazharul@gmail.com>
2023-06-09 14:15:36 +0700

    feat: set provider for react redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
+import { store } from "src/redux/store";
+import { Provider } from "react-redux";
 import React from "react";
+
 import App from "src/App";
 import "src/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
