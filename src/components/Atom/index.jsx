01bd3e1b852fcf9735e578efd59d55ffa7dbf2c8
unknown <zainazharul@gmail.com>
2023-06-09 14:20:50 +0700

    feat: add input export in atom components

--- src/components/Atom/index.jsx
+++ src/components/Atom/index.jsx
@@ -1,3 +1,4 @@
 export { default as Loading } from "src/components/Atom/Loading";
 export { default as Button } from "src/components/Atom/Button";
+export { default as Input } from "src/components/Atom/Input";
 export { default as Logo } from "src/components/Atom/Logo";
