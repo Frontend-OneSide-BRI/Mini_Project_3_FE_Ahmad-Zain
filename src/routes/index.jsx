6be3609d43c13397db8351e814b8bd1b3a091b50
unknown <zainazharul@gmail.com>
2023-06-09 16:05:44 +0700

    feat: add page routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,31 @@
+import MovieDetail from "src/pages/MovieDetail";
+import Movies from "src/pages/Movies";
 import Home from "src/pages/Home";
 
 const routes = [
   {
-    element: <Home />,
-    path: "/",
-    layout: true,
-    access: "guess",
-  },
-  {
     element: <h1>Not Found</h1>,
     path: "*",
     layout: true,
-    access: "guess",
+    // access: "guess",
+  },
+  {
+    element: <Home />,
+    path: "/",
+    layout: true,
+    // access: "guess",
+  },
+  {
+    element: <Movies />,
+    path: "/movies",
+    layout: true,
+    // access: "guess",
+  },
+  {
+    element: <MovieDetail />,
+    path: "/movies/:id",
+    layout: true,
+    // access: "guess",
   },
 ];
 
